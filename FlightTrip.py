89603c024c72556a6723fc0602d4003e623cf330
Mergim <mergimberisha0@gmail.com>
2020-06-05 16:19:19 +0100

    second commit

--- FlightTrip.py
+++ FlightTrip.py
@@ -1,3 +1,5 @@
+from passenger import *
+
 class FlightTrip:
 
     def __init__(self, flight_number, destination, flight_date, flight_time, seats, available_seats, ticket_price, passenger_list=None):
@@ -15,3 +17,25 @@ class FlightTrip:
     def return_passenger_list(self):
         for passenger in self.passenger_list:
             return passenger
+
+
+    def add_to_flight(self, passenger):
+        self.adult_list = []
+        self.infant_list = []
+
+        if passenger.adult == True:
+            self.adult_list.append(passenger)
+
+        else:
+            self.infant_list.append(passenger)
+
+
+    def attendees_list(self):
+        self.passenger_list = self.adult_list + self.infant_list
+
+
+    def ticket_total(self, price):
+        total_revenue = 0
+        for person in self.passenger_list:
+            self.total_revenue += price.passenger_price
+        return total_revenue
