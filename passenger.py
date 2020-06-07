8d3fbece0ad39af1505f65269c632bb89d28ae11
workwithstef <stefan.okolo@outlook.com>
2020-06-07 17:41:54 +0100

    figured out better way to incorporate infants

--- passenger.py
+++ passenger.py
@@ -2,20 +2,47 @@ from people import People
 
 
 class Passenger(People):
-    def __init__(self, name, tax_no, adult, passport_no, d_o_b):
-        super().__init__(name, tax_no, adult)
+    def __init__(self, name, tax_no, passport_no, d_o_b, with_child=False):
+        super().__init__(name, tax_no)
         self.__passport_no = passport_no
         self.__d_o_b = d_o_b
         self.passenger_price = None
+        self.with_child = with_child
+        self.child_name = None
+        self.__child_passport_no = None
+        self.__child_dob = None
         self.set_ticket_price()
+        self.infant_passport()
+        self.infant_name()
+        self.infant_dob()
 
     def set_ticket_price(self):
-        if self.adult:
-            self.passenger_price = 10
+        if self.with_child:
+            self.passenger_price = 15
         else:
-            self.passenger_price = 5
+            self.passenger_price = 10
+
+    def infant_passport(self):
+        if self.with_child:
+            self.__child_passport_no = input(f"Welcome {self.name}, please enter your child's passport number. ")
+
+    def infant_dob(self):
+        if self.with_child:
+            self.__child_dob = input("Please enter your child's Date of Birth. ")
+
+    def infant_name(self):
+        if self.with_child:
+            self.child_name = input("Please enter your child's name. ")
 
     def get_passport_no(self):
         return self.__passport_no
 
+    def get_dob(self):
+        return self.__d_o_b
+
+    def get_infant_pass_no(self):
+        return self.__child_passport_no
+
+    def get_infant_dob(self):
+        return self.__child_dob
 
