44fddd49269f0f3db9204bfd129731e70d67bac1
workwithstef <stefan.okolo@outlook.com>
2020-06-09 00:12:30 +0100

    run file created

--- passenger.py
+++ passenger.py
@@ -4,7 +4,7 @@ from people import People
 class Passenger(People):
     def __init__(self, name, tax_no, passport_no, d_o_b, with_child=False):
         super().__init__(name, tax_no)
-        self.__passport_no = passport_no
+        self.passport_no = passport_no
         self.__d_o_b = d_o_b
         self.passenger_price = None
         self.with_child = with_child
@@ -34,8 +34,8 @@ class Passenger(People):
         if self.with_child:
             self.child_name = input("Please enter your child's name. ")
 
-    def get_passport_no(self):
-        return self.__passport_no
+    # def get_passport_no(self):
+    #     return self.__passport_no
 
     def get_dob(self):
         return self.__d_o_b
